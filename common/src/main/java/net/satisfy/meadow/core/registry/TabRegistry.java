4e56537b7c4eac25e1328f719497bb4f9ebf49e1
satisfy <104036234+satisfyu@users.noreply.github.com>
2025-01-12 21:59:26 +0100

    feat(feature): Spawn Eggs

--- common/src/main/java/net/satisfy/meadow/core/registry/TabRegistry.java
+++ common/src/main/java/net/satisfy/meadow/core/registry/TabRegistry.java
@@ -171,6 +171,7 @@ public class TabRegistry {
                 out.accept(ObjectRegistry.WOODEN_AMETHYST_MILK_BUCKET.get());
                 out.accept(ObjectRegistry.WATER_BUFFALO_SPAWN_EGG_ITEM.get());
                 out.accept(ObjectRegistry.WOOLY_COW_SPAWN_EGG_ITEM.get());
+                out.accept(ObjectRegistry.WOOLY_SHEEP_EGG_ITEM.get());
                 out.accept(ObjectRegistry.PINE_SIGN.get());
                 out.accept(ObjectRegistry.PINE_HANGING_SIGN.get());
                 out.accept(ObjectRegistry.PINE_BOAT.get());
